f6f6abeb8ff3e831702c5dcc73a86bf3a4186236
applese233 <120579968@qq.com>
2025-09-11 09:15:33 +0000

    update

--- scripts/deepscaler/Short-RL.sh
+++ scripts/deepscaler/Short-RL.sh
@@ -1,11 +1,8 @@
-
 MODEL_PATH=Qwen/Qwen2.5-Math-1.5B-Instruct
-
 
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
-export WANDB_API_KEY=4e752bb801154900707afd82eeaccc23c0c9c830
-Name=Qwen-short
+Name=shortRL_qwen1.5B_0911
 SavePath=/home/cliu/deepscaler/checkpoints/$Name
 basepath="/home/cliu/deepscaler/deepscaler/data/orzmath"
 length_tolerance=100
@@ -18,7 +15,7 @@ python3 -m verl.trainer.main_ppo \
     data.val_files=HuggingFaceH4/MATH-500@test \
     data.train_batch_size=128 \
     data.val_batch_size=128 \
-    data.max_prompt_length=1024 \
+    data.max_prompt_length=2048 \
     data.max_response_length=2048 \
     actor_rollout_ref.model.path=$MODEL_PATH  \
     actor_rollout_ref.actor.optim.lr=1e-6 \
@@ -38,19 +35,20 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
-    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.n=4 \
     +actor_rollout_ref.rollout.validate_roll_out_max_length=9216 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
-    trainer.logger=['console','wandb'] \
-    trainer.project_name='DeepScaler' \
+    trainer.logger=['console'] \
+    trainer.project_name='Length-LLM' \
     trainer.experiment_name=$Name \
     trainer.val_before_train=True \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=100 \
+    trainer.save_freq=25 \
     trainer.test_freq=20 \
+    trainer.save_limit=4 \
     trainer.default_hdfs_dir=null \
     trainer.total_epochs=5 "${@:1}" \
     actor_rollout_ref.rollout.enforce_eager=False \
